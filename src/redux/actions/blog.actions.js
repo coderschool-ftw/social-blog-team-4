b9350172cc75623f4f9bc8775a62b9a4703b9410
MinhChau <minhchau3315@gmail.com>
2021-01-28 15:09:44 +0700

    adding basic

---
+++ src/redux/actions/blog.actions.js
@@ -0,0 +1,18 @@
+import * as types from '../constants/blog.constants';
+import api from "../../apiService";
+
+const blogActions = {
+    getBlogs: (pageNum,limit) => async(dispatch) =>{
+        dispatch({type: types.GET_BLOGS_REQUEST});
+        try{
+            let url = `/blogs?page=${pageNum}&limit=${limit}`;
+            const response = await api.get(url);
+            dispatch({type: types.GET_BLOGS_SUCCESS, payload: response.data.data})
+        }
+        catch(error){
+            dispatch({type: types.GET_BLOGS_FAILURE, payload: error})
+        }
+    },
+}
+
+export default blogActions;
