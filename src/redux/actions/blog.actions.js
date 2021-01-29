e43cca0efd025ca1a0a7f8e9702b21eadca363e0
Thang Le Quang <lethang7794@gmail.com>
2021-01-29 18:45:00 +0700

    Refactor blogActions

--- src/redux/actions/blog.actions.js
+++ src/redux/actions/blog.actions.js
@@ -1,17 +1,18 @@
 import * as types from '../constants/blog.constants';
 import api from '../../apiService';
 
+const getBlogs = (pageNum, limit) => async (dispatch) => {
+  dispatch({ type: types.GET_BLOGS_REQUEST });
+  try {
+    let url = `/blogs?page=${pageNum}&limit=${limit}`;
+    const response = await api.get(url);
+    dispatch({ type: types.GET_BLOGS_SUCCESS, payload: response.data.data });
+  } catch (error) {
+    dispatch({ type: types.GET_BLOGS_FAILURE, payload: error });
+  }
+};
 const blogActions = {
-  getBlogs: (pageNum, limit) => async (dispatch) => {
-    dispatch({ type: types.GET_BLOGS_REQUEST });
-    try {
-      let url = `/blogs?page=${pageNum}&limit=${limit}`;
-      const response = await api.get(url);
-      dispatch({ type: types.GET_BLOGS_SUCCESS, payload: response.data.data });
-    } catch (error) {
-      dispatch({ type: types.GET_BLOGS_FAILURE, payload: error });
-    }
-  },
+  getBlogs,
 };
 
 export default blogActions;
