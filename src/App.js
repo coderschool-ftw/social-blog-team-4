d24a4a06490abe8be425fe02f7c8c70eb1cb3e7a
Tia <58371814+thuyet311@users.noreply.github.com>
2021-01-29 14:54:50 +0700

    Add files via upload
    Edit route of AddBlog

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
-import IssueList from "./components/IssueList";
-import SearchForm from "./components/SearchForm";
+import "./App.css";
+import Login from "./pages/Login";
+import PublicNavbar from "./components/PublicNavbar";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminPage from "./pages/AdminPage";
+import AddBlog from "./pages/AddBlog";
+// import BlogDetails from "./pages/BlogDetails";
 
 function App() {
-  const [issues, setIssues] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [url, setUrl] = useState(
-    "https://api.github.com/repos/facebook/react/issues"
-  );
-  useEffect(() => {
-    async function fetchData() {
-      let result = await fetch(url);
-      let json = await result.json();
-      // console.log(json[6].labels[0].name);
-      setIssues(json);
-    }
-    fetchData();
-  }, [url]);
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-  const handleClick = () => {
-    setUrl(`https://api.github.com/repos/${searchTerm}/issues`);
-  };
   return (
-    <Container>
-      <h1 className="text-center text-blue">GitHub Issues Browser</h1>
-      <SearchForm
-        handleClick={handleClick}
-        handleChange={handleChange}
-        searchTerm={searchTerm}
-      ></SearchForm>
+    <div className="App">
+      <PublicNavbar />
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={RegisterPage} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/blog/add" exact component={AddBlog} />
+        {/* <Route path="/blogs/:blog_id" component={BlogDetails} /> */}
 
-      <IssueList issues={issues} />
-    </Container>
+        <ProtectedRoute
+          path="/admin/profile"
+          render={(props) => <AdminPage {...props} />}
+        />
+      </Switch>
+    </div>
   );
 }
 
