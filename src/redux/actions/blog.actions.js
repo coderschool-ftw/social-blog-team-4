c03aaaef62fac70da45b55c32f50aecba0dafb75
Tia <58371814+thuyet311@users.noreply.github.com>
2021-01-29 15:04:55 +0700

    Add files via upload
    Add query

--- src/redux/actions/blog.actions.js
+++ src/redux/actions/blog.actions.js
@@ -1,11 +1,13 @@
-import * as types from '../constants/blog.constants';
-import api from '../../apiService';
+import * as types from "../constants/blog.constants";
+import api from "../../apiService";
 
 const blogActions = {
-  getBlogs: (pageNum, limit) => async (dispatch) => {
+  getBlogs: (pageNum, limit, query) => async (dispatch) => {
     dispatch({ type: types.GET_BLOGS_REQUEST });
     try {
       let url = `/blogs?page=${pageNum}&limit=${limit}`;
+      if (query)
+        url += `&title[$regex]=${query}&title[$options]=i&sortBy[title]=1`;
       const response = await api.get(url);
       dispatch({ type: types.GET_BLOGS_SUCCESS, payload: response.data.data });
     } catch (error) {
