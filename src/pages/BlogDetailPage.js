d06cb6079fc7732883e434cbd6fa909bf67acc2d
Thang Le Quang <lethang7794@gmail.com>
2021-01-30 17:53:26 +0700

    Show reviews, add create review feature in blog detail page

--- src/pages/BlogDetailPage.js
+++ src/pages/BlogDetailPage.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Form, Col, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Moment from 'react-moment';
@@ -10,12 +10,24 @@ const BlogDetailPage = () => {
   const { id } = useParams();
 
   const blog = useSelector((state) => state.blog.blog);
+  let reviews;
+  if (blog && blog.reviews) {
+    reviews = blog.reviews.slice().reverse();
+  }
   const loading = useSelector((state) => state.blog.loading);
+
+  const [review, setReview] = useState('');
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(blogActions.getBlog(id));
   }, [dispatch, id]);
+
+  const handleReviewSubmit = (e, submittedReview, blogId) => {
+    e.preventDefault();
+    setReview('');
+    dispatch(blogActions.submitReview(submittedReview, blogId));
+  };
 
   return (
     <Container className='BlogDetailPage'>
@@ -35,6 +47,43 @@ const BlogDetailPage = () => {
               </div>
 
               <Reactions blog={blog} dispatch={dispatch} id={id} />
+
+              <Form
+                onSubmit={(e) => {
+                  console.log('Hello');
+                  handleReviewSubmit(e, review, id);
+                }}
+              >
+                <Form.Row className='align-items-center'>
+                  <Col sm={9} className='my-1'>
+                    <Form.Label htmlFor='inlineFormInputReview' srOnly>
+                      Review
+                    </Form.Label>
+                    <Form.Control
+                      type='text'
+                      placeholder='Wanna say something?'
+                      value={review}
+                      onChange={(e) => setReview(e.target.value)}
+                    />
+                  </Col>
+                  <Col xs='auto' className='my-1'>
+                    <Button type='submit'>Review</Button>
+                  </Col>
+                </Form.Row>
+              </Form>
+
+              <ul>
+                {reviews.map((r) => (
+                  <li key={r._id}>
+                    <strong>{r.user.name}</strong>
+                    <span> says </span>
+                    <strong>{r.content}</strong>
+                    <span>
+                      , <Moment fromNow>{r.createdAt}</Moment>
+                    </span>
+                  </li>
+                ))}
+              </ul>
             </>
           )}
         </>
