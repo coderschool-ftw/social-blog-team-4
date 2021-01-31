8d63aaf349d7ea8fc3e9c342e116ee8346150e9b
Tia <58371814+thuyet311@users.noreply.github.com>
2021-01-31 22:51:14 +0700

    Update JumbotronBlog.js

--- src/components/JumbotronBlog.js
+++ src/components/JumbotronBlog.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import "../App.css";
 
 const JumbotronBlog = () => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   return (
-    <Jumbotron className='text-center'>
+    <Jumbotron className='text-center jumbotron-bg'>
       <h1>Social Blog</h1>
       <p>Write about your amazing experiences.</p>
       {isAuthenticated && (
